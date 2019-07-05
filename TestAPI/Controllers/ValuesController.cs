7976fc54890ab59a7934e0cc8401e6106dd6be2d
dong <d18736988716@outlook.com>
2019-07-05 16:39:03 +0800

    测试接口重新提交

---
+++ TestAPI/Controllers/ValuesController.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcMediaService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+
+        #region FormPublic(会刊)
+
+        [Route("createFormPublicInfo")]
+        [HttpPost]
+        public IActionResult createFormPublicInfo(FormPublicStruct model)
+        {
+            var result = EGClient.createFormPublicInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateFormPublicInfo")]
+        [HttpPost]
+        public IActionResult updateFormPublicInfo(FormPublicStruct model)
+        {
+            var result = EGClient.updateFormPublicInfo(model);
+            return Ok(result);
+        }
+
+        [Route("deleteFormPublicInfoById")]
+        [HttpPost]
+        public IActionResult deleteFormPublicInfoById(IdRequest IdRequest)
+        {
+            var result = EGClient.deleteFormPublicInfoById(IdRequest);
+            return Ok(result);
+        }
+
+        [Route("getFormPublicInfoList")]
+        [HttpPost]
+        public IActionResult getFormPublicInfoList(Empty empty)
+        {
+            var result = EGClient.getFormPublicInfoList(empty);
+            return Ok(result);
+        }
+
+        [Route("getFormPublicInfoById")]
+        [HttpPost]
+        public IActionResult getFormPublicInfoById(IdRequest IdRequest)
+        {
+            var result = EGClient.getFormPublicInfoById(IdRequest);
+            return Ok(result);
+        }
+
+        [Route("getFormPublicList")]
+        [HttpPost]
+        public IActionResult getFormPublicList(PaginationRequestSearch search)
+        {
+            var result = EGClient.getFormPublicList(search);
+            return Ok(result);
+        }
+        #endregion
+
+
+
+
+        #region Express(快递单)
+
+        [Route("createExpressInfo")]
+        [HttpPost]
+        public IActionResult createExpressInfo(ExpressStruct model)
+        {
+            var result = EGClient.createExpressInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateExpressInfo")]
+        [HttpPost]
+        public IActionResult updateExpressInfo(ExpressStruct model)
+        {
+            var result = EGClient.updateExpressInfo(model);
+            return Ok(result);
+        }
+
+
+        [Route("deleteExpressInfoById")]
+        [HttpPost]
+        public IActionResult deleteExpressInfoById(IdRequest idRequest)
+        {
+            var result = EGClient.deleteExpressInfoById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getExpressInfoById")]
+        [HttpPost]
+        public IActionResult getExpressInfoById(IdRequest idRequest)
+        {
+            var result = EGClient.getExpressInfoById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getExpressList")]
+        [HttpPost]
+
+        public IActionResult getExpressList(PaginationRequestSearch search)
+        {
+            var result = EGClient.getExpressList(search);
+            return Ok(result);
+        }
+
+
+        #endregion
+
+
+        #region CatalogueBooks(会刊订购信息)
+        [Route("getCatalogueBooks")]
+        [HttpPost]
+        public IActionResult getCatalogueBooks(PaginationRequestSearch search)
+        {
+            var result = EGClient.GetAllCatalogueBooksList(search);
+
+            return Ok(result);
+        }
+
+        [Route("createCatalogueBooksInfo")]
+        [HttpPost]
+        public IActionResult createCatalogueBooksInfo(CatalogueBooksStruct model)
+        {
+            var result = EGClient.CreateCatalogueBooksInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateCatalogueBooksInfo")]
+        [HttpPost]
+        public IActionResult updateCatalogueBooksInfo(CatalogueBooksStruct model)
+        {
+            var result = EGClient.updateCatalogueBooksInfo(model);
+            return Ok(result);
+        }
+
+        [Route("deleteCatalogueBooksById")]
+        [HttpPost]
+        public IActionResult deleteCatalogueBooksById(IdRequest idRequest)
+        {
+            var result = EGClient.deleteCatalogueBooksById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getCatalogueBooksById")]
+        [HttpPost]
+        public IActionResult getCatalogueBooksById(IdRequest idRequest)
+        {
+            var result = EGClient.getCatalogueBooksById(idRequest);
+            return Ok(result);
+        }
+        #endregion
+
+
+        #region Interview(专题采访)
+
+        [Route("createInterviewInfo")]
+        [HttpPost]
+        public IActionResult createInterviewInfo(InterviewStruct model)
+        {
+            var result = EGClient.createInterviewInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateInterviewInfo")]
+        [HttpPost]
+        public IActionResult updateInterviewInfo(InterviewStruct model)
+        {
+            var result = EGClient.updateInterviewInfo(model);
+            return Ok(result);
+        }
+
+        [Route("deleteInterviewById")]
+        [HttpPost]
+        public IActionResult deleteInterviewById(IdRequest idRequest)
+        {
+            var result = EGClient.deleteInterviewById(idRequest);
+            return Ok(result);
+        }
+
+
+        [Route("getInterviewInfoById")]
+        [HttpPost]
+        public IActionResult getInterviewInfoById(IdRequest idRequest)
+        {
+            var result = EGClient.getInterviewInfoById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getInterviewList")]
+        [HttpPost]
+        public IActionResult getInterviewList(PaginationRequestSearch search)
+        {
+            var result = EGClient.getInterviewList(search);
+            return Ok(result);
+        }
+
+        #endregion
+
+
+
+        #region  HighlightsInfo(十大亮点申请)
+
+        [Route("createHighlightsInfo")]
+        [HttpPost]
+        public IActionResult createHighlightsInfo(HighlightsInfoStruct model)
+        {
+            var result = EGClient.createHighlightsInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateHighlightsInfo")]
+        [HttpPost]
+        public IActionResult updateHighlightsInfo(HighlightsInfoStruct model)
+        {
+            var result = EGClient.updateHighlightsInfo(model);
+            return Ok(result);
+        }
+
+        [Route("deleteHighlightsInfoById")]
+        [HttpPost]
+        public IActionResult deleteHighlightsInfoById(IdRequest idRequest)
+        {
+            var result = EGClient.deleteHighlightsInfoById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getHighlightsInfoById")]
+        [HttpPost]
+        public IActionResult getHighlightsInfoById(IdRequest idRequest)
+        {
+            var result = EGClient.getHighlightsInfoById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getHighlightsInfoList")]
+        [HttpPost]
+        public IActionResult getHighlightsInfoList(PaginationRequestSearch search)
+        {
+            var result = EGClient.getHighlightsInfoList(search);
+            return Ok(result);
+        }
+
+        #endregion
+
+
+
+        #region Hotel(酒店)
+
+        [Route("createHotelInfo")]
+        [HttpPost]
+        public IActionResult createHotelInfo(HotelStruct model)
+        {
+            var result = EGClient.createHotelInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateHotelInfo")]
+        [HttpPost]
+        public IActionResult updateHotelInfo(HotelStruct model)
+        {
+            var result = EGClient.updateHotelInfo(model);
+            return Ok(result);
+        }
+
+        [Route("deleteHotelInfoById")]
+        [HttpPost]
+        public IActionResult deleteHotelInfoById(HotelId idRequest)
+        {
+            var result = EGClient.deleteHotelInfoById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getHotelById")]
+        [HttpPost]
+        public IActionResult getHotelById(HotelId idRequest)
+        {
+            var result = EGClient.getHotelById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getHotelList")]
+        [HttpPost]
+        public IActionResult getHotelList(Empty empty)
+        {
+            var result = EGClient.getHotelList(empty);
+            return Ok(result);
+        }
+
+        #endregion
+
+
+
+        #region HotelRoomType(酒店房间类型)
+
+        [Route("createHotelRoomTypeInfo")]
+        [HttpPost]
+        public IActionResult createHotelRoomTypeInfo(HotelRoomTypeStruct model)
+        {
+            var result = EGClient.createHotelRoomTypeInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateHotelRoomTypeInfo")]
+        [HttpPost]
+        public IActionResult updateHotelRoomTypeInfo(HotelRoomTypeStruct model)
+        {
+            var result = EGClient.updateHotelRoomTypeInfo(model);
+            return Ok(result);
+        }
+
+        [Route("deleteHotelRoomTypeById")]
+        [HttpPost]
+        public IActionResult deleteHotelRoomTypeById(HotelRoomTypeId hotelRoomTypeId)
+        {
+            var result = EGClient.deleteHotelRoomTypeById(hotelRoomTypeId);
+            return Ok(result);
+        }
+
+        [Route("getHotelRoomTypeInfoById")]
+        [HttpPost]
+        public IActionResult getHotelRoomTypeInfoById(HotelRoomTypeId hotelRoomTypeId)
+        {
+            var result = EGClient.getHotelRoomTypeInfoById(hotelRoomTypeId);
+            return Ok(result);
+        }
+
+        [Route("getHolteRoomTypeListByHotelId")]
+        [HttpPost]
+        public IActionResult getHolteRoomTypeListByHotelId(HotelId hotelId)
+        {
+            var result = EGClient.getHolteRoomTypeListByHotelId(hotelId);
+            return Ok(result);
+        }
+
+        #endregion
+
+
+
+        #region HotelBookRecord(酒店预订记录)
+
+        [Route("createHotelBookRecordInfo")]
+        [HttpPost]
+        public IActionResult createHotelBookRecordInfo(HotelBookRecordStruct model)
+        {
+            var result = EGClient.createHotelBookRecordInfo(model);
+            return Ok(result);
+        }
+
+        [Route("updateHotelBookRecordInfo")]
+        [HttpPost]
+        public IActionResult updateHotelBookRecordInfo(HotelBookRecordStruct model)
+        {
+            var result = EGClient.updateHotelBookRecordInfo(model);
+            return Ok(result);
+        }
+
+
+        [Route("deleteHotelBookRecordInfoById")]
+        [HttpPost]
+        public IActionResult deleteHotelBookRecordInfoById(IdRequest idRequest)
+        {
+            var result = EGClient.deleteHotelBookRecordInfoById(idRequest);
+            return Ok(result);
+        }
+
+
+        [Route("getHotelBookRecordByMemberId")]
+        [HttpPost]
+        public IActionResult getHotelBookRecordByMemberId(MemberId memberId)
+        {
+            var result = EGClient.getHotelBookRecordByMemberId(memberId);
+            return Ok(result);
+        }
+
+
+        [Route("cancelHotelBookRecordById")]
+        [HttpPost]
+        public IActionResult cancelHotelBookRecordById(IdRequest idRequest)
+        {
+            var result = EGClient.cancelHotelBookRecordById(idRequest);
+            return Ok(result);
+        }
+
+
+        [Route("getHotelBookRecordById")]
+        [HttpPost]
+        public IActionResult getHotelBookRecordById(IdRequest idRequest)
+        {
+            var result = EGClient.getHotelBookRecordById(idRequest);
+            return Ok(result);
+        }
+
+        [Route("getHotelBookRecordList")]
+        [HttpPost]
+        public IActionResult getHotelBookRecordList(PaginationRequestSearch search)
+        {
+            var result = EGClient.getHotelBookRecordList(search);
+            return Ok(result);
+        }
+
+        #endregion
+
+       
+
+    }
+
+
+}
---
+++ TestAPI/EGClient.cs
@@ -0,0 +1,312 @@
+﻿
+using Grpc.Core;
+using GrpcMediaService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI
+{
+    public class EGClient
+    {
+        private static Channel _channel;
+        private static MediaServiceToGrpc.MediaServiceToGrpcClient _client;
+
+        static EGClient()
+        {
+            _channel = new Channel("mediaservice:40001", ChannelCredentials.Insecure);
+            _client = new MediaServiceToGrpc.MediaServiceToGrpcClient(_channel);
+        }
+
+        #region FormPublic(会刊)
+
+        public static ModifyReply createFormPublicInfo(FormPublicStruct model)
+        {
+            return _client.createFormPublicInfo(model);
+        }
+
+
+        public static ModifyReply updateFormPublicInfo(FormPublicStruct model)
+        {
+            return _client.updateFormPublicInfo(model);
+        }
+
+        public static ModifyReply deleteFormPublicInfoById(IdRequest IdRequest)
+        {
+            return _client.deleteFormPublicInfoById(IdRequest);
+        }
+
+        public static FormPublicList getFormPublicInfoList(Empty empty)
+        {
+            return _client.getFormPublicInfoList(empty);
+        }
+
+        public static FormPublicStruct getFormPublicInfoById(IdRequest IdRequest)
+        {
+            return _client.getFormPublicInfoById(IdRequest);
+        }
+
+        public static FormPublicList getFormPublicList(PaginationRequestSearch search)
+        {
+            return _client.getFormPublicList(search);
+        }
+        #endregion
+
+
+
+
+        #region Express(快递单)
+
+        public static ModifyReply createExpressInfo(ExpressStruct model)
+        {
+            return _client.createExpressInfo(model);
+        }
+
+
+        public static ModifyReply updateExpressInfo(ExpressStruct model)
+        {
+            return _client.updateExpressInfo(model);
+        }
+
+
+
+        public static ModifyReply deleteExpressInfoById(IdRequest idRequest)
+        {
+            return _client.deleteExpressInfoById(idRequest);
+        }
+
+
+
+        public static ExpressStruct getExpressInfoById(IdRequest idRequest)
+        {
+            return _client.getExpressInfoById(idRequest);
+        }
+
+
+
+        public static ExpressList getExpressList(PaginationRequestSearch search)
+        {
+            return _client.getExpressList(search);
+        }
+
+
+
+
+
+        #endregion
+
+
+        #region CatalogueBooks(会刊订购信息)
+
+        public static CatalogueBooksList GetAllCatalogueBooksList(PaginationRequestSearch search)
+        {
+            return _client.getCatalogueBooksList(search);
+        }
+
+        public static ModifyReply CreateCatalogueBooksInfo(CatalogueBooksStruct model)
+        {
+            return _client.createCatalogueBooksInfo(model);
+        }
+
+        public static ModifyReply updateCatalogueBooksInfo(CatalogueBooksStruct model)
+        {
+            return _client.updateCatalogueBooksInfo(model);
+        }
+
+        public static ModifyReply deleteCatalogueBooksById(IdRequest idRequest)
+        {
+            return _client.deleteCatalogueBooksById(idRequest);
+        }
+
+        public static CatalogueBooksStruct getCatalogueBooksById(IdRequest idRequest)
+        {
+            return _client.getCatalogueBooksById(idRequest);
+        }
+        #endregion
+
+
+
+        #region Interview(专题采访)
+
+        public static ModifyReply createInterviewInfo(InterviewStruct model)
+        {
+            return _client.createInterviewInfo(model);
+        }
+
+
+        public static ModifyReply updateInterviewInfo(InterviewStruct model)
+        {
+            return _client.updateInterviewInfo(model);
+        }
+
+
+        public static ModifyReply deleteInterviewById(IdRequest idRequest)
+        {
+            return _client.deleteInterviewById(idRequest);
+        }
+
+
+        public static InterviewStruct getInterviewInfoById(IdRequest idRequest)
+        {
+            return _client.getInterviewInfoById(idRequest);
+        }
+
+
+        public static InterviewList getInterviewList(PaginationRequestSearch search)
+        {
+            return _client.getInterviewList(search);
+        }
+
+        #endregion
+
+
+
+        #region  HighlightsInfo(十大亮点申请)
+
+        public static ModifyReply createHighlightsInfo(HighlightsInfoStruct model)
+        {
+            return _client.createHighlightsInfo(model);
+        }
+
+        public static ModifyReply updateHighlightsInfo(HighlightsInfoStruct model)
+        {
+            return _client.updateHighlightsInfo(model);
+        }
+
+        public static ModifyReply deleteHighlightsInfoById(IdRequest idRequest)
+        {
+            return _client.deleteHighlightsInfoById(idRequest);
+        }
+
+
+        public static HighlightsInfoStruct getHighlightsInfoById(IdRequest idRequest)
+        {
+            return _client.getHighlightsInfoById(idRequest);
+        }
+
+
+        public static HighlightsInfoList getHighlightsInfoList(PaginationRequestSearch search)
+        {
+            return _client.getHighlightsInfoList(search);
+        }
+
+        #endregion
+
+
+
+        #region Hotel(酒店)
+
+
+        public static ModifyReply createHotelInfo(HotelStruct model)
+        {
+            return _client.createHotelInfo(model);
+        }
+
+
+        public static ModifyReply updateHotelInfo(HotelStruct model)
+        {
+            return _client.updateHotelInfo(model);
+        }
+
+
+        public static ModifyReply deleteHotelInfoById(HotelId idRequest)
+        {
+            return _client.deleteHotelInfoById(idRequest);
+        }
+
+
+        public static HotelStruct getHotelById(HotelId idRequest)
+        {
+            return _client.getHotelById(idRequest);
+        }
+
+        public static HotelList getHotelList(Empty empty)
+        {
+            return _client.getHotelList(empty);
+        }
+
+        #endregion
+
+
+
+        #region HotelRoomType(酒店房间类型)
+
+        public static ModifyReply createHotelRoomTypeInfo(HotelRoomTypeStruct model)
+        {
+            return _client.createHotelRoomTypeInfo(model);
+        }
+
+        public static ModifyReply updateHotelRoomTypeInfo(HotelRoomTypeStruct model)
+        {
+            return _client.updateHotelRoomTypeInfo(model);
+        }
+
+
+        public static ModifyReply deleteHotelRoomTypeById(HotelRoomTypeId hotelRoomTypeId)
+        {
+            return _client.deleteHotelRoomTypeById(hotelRoomTypeId);
+        }
+
+
+        public static HotelRoomTypeStruct getHotelRoomTypeInfoById(HotelRoomTypeId hotelRoomTypeId)
+        {
+            return _client.getHotelRoomTypeInfoById(hotelRoomTypeId);
+        }
+
+
+        public static HotelRoomTypeList getHolteRoomTypeListByHotelId(HotelId hotelId)
+        {
+            return _client.getHolteRoomTypeListByHotelId(hotelId);
+        }
+
+        #endregion
+
+
+
+        #region HotelBookRecord(酒店预订记录)
+
+        public static ModifyReply createHotelBookRecordInfo(HotelBookRecordStruct model)
+        {
+            return _client.createHotelBookRecordInfo(model);
+        }
+
+        public static ModifyReply updateHotelBookRecordInfo(HotelBookRecordStruct model)
+        {
+            return _client.updateHotelBookRecordInfo(model);
+        }
+
+        public static ModifyReply deleteHotelBookRecordInfoById(IdRequest idRequest)
+        {
+            return _client.deleteHotelBookRecordInfoById(idRequest);
+        }
+
+
+        public static HotelBookRecordList getHotelBookRecordByMemberId(MemberId memberId)
+        {
+            return _client.getHotelBookRecordByMemberId(memberId);
+        }
+
+
+        public static ModifyReply cancelHotelBookRecordById(IdRequest idRequest)
+        {
+            return _client.cancelHotelBookRecordById(idRequest);
+        }
+
+
+        public static HotelBookRecordStruct getHotelBookRecordById(IdRequest idRequest)
+        {
+            return _client.getHotelBookRecordById(idRequest);
+        }
+
+
+        public static HotelBookRecordList getHotelBookRecordList(PaginationRequestSearch search)
+        {
+            return _client.getHotelBookRecordList(search);
+        }
+
+        #endregion
+
+
+    }
+}
