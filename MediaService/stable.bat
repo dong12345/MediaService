d406c78b1fdb609cd47dd15439d5e9ec7b0f7505
dong <d18736988716@outlook.com>
2019-12-25 17:17:01 +0800

    添加正式上传文件

--- MediaService/stable.bat
+++ MediaService/stable.bat
@@ -1,5 +1,6 @@
+
 dotnet publish -c Release -r linux-x64
-docker build -f DockerfilePublish_stable -t 47.104.249.194:5000/entranceguard.api:latest .
-docker push 47.104.249.194:5000/entranceguard.api:latest
-curl -u onlinetest:564b8580782317fc7c85dc7846251251 http://47.104.249.194:7500/job/SNEC_ONLINE_DEBUG/build?token=rebuildtest
+docker build -f DockerfilePublish_stable -t 47.104.249.194:5000/mediaservice.api:stable .
+docker push 47.104.249.194:5000/mediaservice.api:stable
+curl -u onlinetest:564b8580782317fc7c85dc7846251251 http://47.104.249.194:7500/job/MIRROR_PRODUCTION/build?token=MIRROR_PRODUCTION
 pause
